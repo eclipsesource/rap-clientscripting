486b3f860a67e70ee7a0f4de98350df267fcff09
Ralf Sternberg <rsternberg@eclipsesource.com>
2012-02-04 00:20:52 +0100

    Added client-side protocol adapters

---
+++ org.eclipse.rap.clientscripting/js/org/eclipse/rap/clientscripting/ListenerAdapter.js
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2012 EclipseSource and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    EclipseSource - initial API and implementation
+ ******************************************************************************/
+
+org.eclipse.rwt.protocol.AdapterRegistry.add( "rwt.clientscripting.Listener", {
+
+  factory : function( properties ) {
+  	var code = properties.code;
+    return new org.eclipse.rap.clientscripting.Function( code );
+  },
+
+  destructor : qx.lang.Function.returnTrue,
+
+  properties : [],
+
+  propertyHandler : {},
+
+  listeners : [],
+
+  listenerHandler : {},
+
+  methods : [],
+
+  methodHandler : {}
+
+} );
