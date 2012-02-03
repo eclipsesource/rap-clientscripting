290cb4592f7b5143af3f389abf36497823d1ba76
Ralf Sternberg <rsternberg@eclipsesource.com>
2012-02-03 11:48:46 +0100

    Split demo code into smaller methods

--- org.eclipse.rap.clientscripting.demo/src/org/eclipse/rap/clientscripting/Demo.java
+++ org.eclipse.rap.clientscripting.demo/src/org/eclipse/rap/clientscripting/Demo.java
@@ -12,62 +12,24 @@ import org.eclipse.rwt.lifecycle.WidgetUtil;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.Widget;
 
-@SuppressWarnings("restriction")
+@SuppressWarnings( { "restriction", "serial" } )
 public class Demo implements IEntryPoint {
-    
+
   public int createUI() {
     Display display = new Display();
-    final Shell shell = new Shell( display );
-    shell.setLayout( new GridLayout( 2, false ) );
-    
-    final Text date = new Text( shell, SWT.SINGLE );
-    date.setText( "__.__.____ " ); // last empty space prevents horizontal scrolling
-    addDateMaskBehavior( date );
-    
-    Button verify = new Button( shell, SWT.PUSH );
-    verify.setText( "Verify" );
-    verify.addSelectionListener( new SelectionAdapter() {
-      public void widgetSelected( SelectionEvent e ) {
-        String result = date.getText();
-        String[] values = result.split( "\\.", 3 );
-        String message = "Date OK";
-        int kind = MessageDialog.CONFIRM;
-        try {
-          if( Integer.parseInt( values[ 0 ] ) > 31 ) {
-            kind = MessageDialog.ERROR;
-            message = "Incorrect day " + values[ 0 ];
-          }
-          if( Integer.parseInt( values[ 1 ] ) > 12 ) {
-            kind = MessageDialog.ERROR;
-            message = "Incorrect month " + values[ 1 ];
-          }
-          Integer.parseInt( values[ 2 ].trim() ); // remove trailing " "
-        } catch( NumberFormatException ex ) {
-          kind = MessageDialog.ERROR;
-          message = "Incomplete";
-        }
-        MessageDialog.open( kind, shell, "Verify", message, SWT.NONE );
-      }
-    } );
-    
-    Label count = new Label( shell, SWT.PUSH );
-    count.setText( "Click!    " ); // empty space because text set on client is longer 
-    addCounterBehavior( count );
-
-    Button logger = new Button( shell, SWT.PUSH );
-    logger.setText( "This logs all events" ); 
-    addLoggerBehavior( logger );
-    
-    shell.pack();
+    Shell shell = new Shell( display );
+    createShellContents( shell );
+    shell.setBounds( 20, 20, 400, 200 );
     shell.open();
     while( !shell.isDisposed() ) {
       if( !display.readAndDispatch() ) {
@@ -76,6 +38,47 @@ public class Demo implements IEntryPoint {
     }
     display.dispose();
     return 0;
+  }
+
+  private void createShellContents( Composite parent ) {
+    parent.setLayout( new GridLayout( 2, false ) );
+    Text dateField = createDateField( parent );
+    dateField.setLayoutData( new GridData( SWT.FILL, SWT.CENTER, true, false ) );
+    createVerifyButton( parent, dateField );
+    Label counterLabel = createCounterLabel( parent );
+    counterLabel.setLayoutData( new GridData( SWT.FILL, SWT.CENTER, true, false ) );
+    createLoggerButton( parent );
+  }
+
+  private Text createDateField( Composite parent ) {
+    Text date = new Text( parent, SWT.SINGLE | SWT.BORDER );
+    date.setText( "__.__.____" );
+    addDateMaskBehavior( date );
+    return date;
+  }
+
+  private void createVerifyButton( Composite parent, final Text dateField ) {
+    Button verify = new Button( parent, SWT.PUSH );
+    verify.setText( "Verify" );
+    verify.addSelectionListener( new SelectionAdapter() {
+      @Override
+      public void widgetSelected( SelectionEvent e ) {
+        verifyDate( dateField );
+      }
+    } );
+  }
+
+  private Label createCounterLabel( final Composite parent ) {
+    Label label = new Label( parent, SWT.PUSH );
+    label.setText( "Click!" );
+    addCounterBehavior( label );
+    return label;
+  }
+
+  private void createLoggerButton( final Composite parent ) {
+    Button logger = new Button( parent, SWT.PUSH );
+    logger.setText( "This logs all events" );
+    addLoggerBehavior( logger );
   }
 
   private void addDateMaskBehavior( Text text ) {
@@ -89,6 +92,28 @@ public class Demo implements IEntryPoint {
     code += getWidgetRef( text );
     code += ", org.eclipse.rap.clientscripting.SWT.MouseDown, f );";
     JSExecutor.executeJS( code );
+  }
+
+  private void verifyDate( Text dateField ) {
+    String text = dateField.getText();
+    String[] values = text.split( "\\.", 3 );
+    String message = "Date OK";
+    int kind = MessageDialog.CONFIRM;
+    try {
+      if( Integer.parseInt( values[ 0 ] ) > 31 ) {
+        kind = MessageDialog.ERROR;
+        message = "Incorrect day " + values[ 0 ];
+      }
+      if( Integer.parseInt( values[ 1 ] ) > 12 ) {
+        kind = MessageDialog.ERROR;
+        message = "Incorrect month " + values[ 1 ];
+      }
+      Integer.parseInt( values[ 2 ].trim() ); // remove trailing " "
+    } catch( NumberFormatException ex ) {
+      kind = MessageDialog.ERROR;
+      message = "Incomplete";
+    }
+    MessageDialog.open( kind, dateField.getShell(), "Verify", message, SWT.NONE );
   }
 
   private void addCounterBehavior( Label count ) {
@@ -105,10 +130,10 @@ public class Demo implements IEntryPoint {
     String code = "var f = new org.eclipse.rap.clientscripting.Function( \"";
     code += getJSHandler( "Logger.js" );
     code += "\" );";
-    String[] events = { 
-      "SWT.KeyDown", 
-      "SWT.KeyUp", 
-      "SWT.FocusIn", 
+    String[] events = {
+      "SWT.KeyDown",
+      "SWT.KeyUp",
+      "SWT.FocusIn",
       "SWT.FocusOut",
       "SWT.MouseDown",
       "SWT.MouseUp",
@@ -124,7 +149,7 @@ public class Demo implements IEntryPoint {
     }
     JSExecutor.executeJS( code );
   }
-  
+
   private String getWidgetRef( Widget widget ) {
     String result = "org.eclipse.rwt.protocol.ObjectManager.getObject( \"";
     result += WidgetUtil.getId( widget );
@@ -161,7 +186,7 @@ public class Demo implements IEntryPoint {
     } else {
       buffer = null;
     }
-    return buffer;    
+    return buffer;
   }
 
 }
